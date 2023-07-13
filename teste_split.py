428d92c1a97505b169ad1c89561c130f43e2ffed
Matheus Richard <106637514+MatheusRw@users.noreply.github.com>
2023-07-13 19:16:38 -0300

    Update teste_split.py

--- teste_split.py
+++ teste_split.py
@@ -5,7 +5,7 @@ def split_excel_file(input_file, output_dir):
     for i in range(0, df.shape[0], 1000):
         chunk = df[i:i+1000]
         output_file = os.path.join(output_dir, f"{i}.csv")
-        chunk.to_excel(output_file)
+        chunk.to_csv(output_file)
 
 if __name__ == "__main__":
     input_file = input("Enter the input Excel file: ")
